2403843d1d1e191b471b825883f01a356f732f0f
Duan0603 <hoangduan06032005@gmail.com>
2025-07-14 21:06:01 +0700

    delete unuse

--- src/model/DifficultyLevels.java
+++
@@ -1,8 +0,0 @@
-package model;
-
-public final class DifficultyLevels {
-    public static final String BEGINNER = "Cơ bản";
-    public static final String INTERMEDIATE = "Trung cấp";
-    public static final String ADVANCED = "Nâng cao";
-    private DifficultyLevels() {}
-}
